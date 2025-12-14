05ee45fb4ede1d13e0cc1cfd8b3c01b393018210
mihindugunarathne <mihindupramudith@gmail.com>
2025-12-14 21:10:48 +0530

    create the navbar and setup the clerk authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/student/Home.jsx'
+import CoursesList from './pages/student/CoursesList.jsx'
+import CourseDetails from './pages/student/CourseDetails.jsx'
+import MyEnrollments from './pages/student/MyEnrollments.jsx'
+import Player from './pages/student/Player.jsx'
+import Loading from './components/student/Loading.jsx'
+import Educator from './pages/educator/Educator.jsx'
+import Dashboard from './pages/educator/Dashboard.jsx'
+import AddCourse from './pages/educator/AddCourse.jsx'
+import MyCourses from './pages/educator/MyCourses.jsx'
+import StudentEnrolled from './pages/educator/StudentEnrolled.jsx'
+import Navbar from './components/student/Navbar.jsx'
+import { useMatch } from 'react-router-dom'
+
 
 const App = () => {
+
+  const isEducatorRoute = useMatch('/educator/*')
+
   return (
-    <div>
-      
+    <div className='text-default min-h-screen bg-white'>
+      {!isEducatorRoute && <Navbar />}
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/course-list" element={<CoursesList />} />
+         <Route path="/course-list/:input" element={<CoursesList />} />
+         <Route path="/course/:id" element={<CourseDetails />} />
+         <Route path="/my-enrollments" element={<MyEnrollments />} />
+         <Route path="/player/:courseId" element={<Player />} />
+         <Route path="/loading/:path" element={<Loading />} />
+         <Route path="/educator" element={<Educator/>} >
+            <Route path='educator' element={<Dashboard/>}/>
+            <Route path='add-course' element={<AddCourse/>}/>
+            <Route path='my-courses' element={<MyCourses/>}/>
+            <Route path='student-enrolled' element={<StudentEnrolled/>}/>
+         </Route>
+       </Routes>
     </div>
   )
 }
